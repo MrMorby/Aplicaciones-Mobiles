d431aa3ee5a01adb70b33944c29a6c0921d00f54
Bastián Heresmann <emiliarwr@gmail.com>
2024-10-14 06:10:13 -0300

    api google

--- src/app/main/main.page.ts
+++ src/app/main/main.page.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { AnimationController } from '@ionic/angular';
+
+import { Component, AfterViewInit } from '@angular/core';
+
+declare var google: any;
 
 @Component({
   selector: 'app-main',
@@ -29,4 +31,18 @@ export class MainPage {
   animation: any;
 
   constructor() {}
+
+  ngAfterViewInit() {
+    this.loadMap();
+  }
+
+  loadMap() {
+    const mapOptions = {
+      center: new google.maps.LatLng(-33.447487, -70.673676),  // Ejemplo: Santiago, Chile
+      zoom: 12,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    const map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  }
+
 }
