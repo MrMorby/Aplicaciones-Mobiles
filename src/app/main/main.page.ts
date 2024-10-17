9733e6bfca7611dffef4f36f4503412b84ff21e0
Bastián Heresmann <emiliarwr@gmail.com>
2024-10-17 19:56:45 -0300

    api de localización

--- src/app/main/main.page.ts
+++ src/app/main/main.page.ts
@@ -1,5 +1,6 @@
 
 import { Component, AfterViewInit } from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';  // Importa el plugin de Geolocation
 
 declare var google: any;
 
@@ -27,20 +28,41 @@ export class MainPage {
       price: '$200.00'
     }
   ];
+  latitude: number;
+  longitude: number;
 
   constructor() {}
 
   ngAfterViewInit() {
-    this.loadMap();
+    //this.loadMap();
+    this.getUserLocation();
   }
 
-  loadMap() {
+  // Función para obtener la ubicación del usuario
+  async getUserLocation() {
+    try {
+      const coordinates = await Geolocation.getCurrentPosition();
+      this.latitude = coordinates.coords.latitude;
+      this.longitude = coordinates.coords.longitude;
+      console.log('Ubicación actual:', this.latitude, this.longitude);
+
+      // Una vez obtenida la ubicación, cargamos el mapa centrado en la ubicación actual
+      this.loadMap(this.latitude, this.longitude);
+
+    } catch (error) {
+      console.error('Error al obtener la ubicación:', error);
+      // Si hay algún error, podemos cargar el mapa con una ubicación por defecto
+      this.loadMap(-33.447487, -70.673676);  // Santiago, Chile (por defecto)
+    }
+  }
+
+  // Modificamos la función loadMap para aceptar latitud y longitud como parámetros
+  loadMap(lat: number, lng: number) {
     const mapOptions = {
-      center: new google.maps.LatLng(-33.447487, -70.673676),  // Ejemplo: Santiago, Chile
+      center: new google.maps.LatLng(lat, lng),  // Centramos el mapa en la ubicación actual
       zoom: 12,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     const map = new google.maps.Map(document.getElementById('map'), mapOptions);
   }
-
 }
