096aab5ba8bc9054040e02b2590202653ee06cd6
Gyauss <diegoacevedo.esteban@gmail.com>
2024-10-24 03:09:41 -0300

    VALIDADORES CREATE product

--- src/app/publicate-product/publicate-product.module.ts
+++ src/app/publicate-product/publicate-product.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; // importa ReactiveFormsModule
 import { IonicModule } from '@ionic/angular';
 
 import { PublicateProductPageRoutingModule } from './publicate-product-routing.module';
@@ -12,6 +11,7 @@ import { PublicateProductPage } from './publicate-product.page';
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule, // importa ReactiveFormsModule aquí
     IonicModule,
     PublicateProductPageRoutingModule
   ],
--- src/app/publicate-product/publicate-product.page.ts
+++ src/app/publicate-product/publicate-product.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-publicate-product',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./publicate-product.page.scss'],
 })
 export class PublicateProductPage implements OnInit {
+  productoForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder) {
+    // inicializamos los controles del formulario con validadores
+    this.productoForm = this.formBuilder.group({
+      nombre: ['', [Validators.required, Validators.minLength(3)]],
+      precio: ['', [Validators.required, Validators.min(1)]],
+      descripcion: ['', [Validators.required, Validators.minLength(10)]],
+    });
   }
 
+  ngOnInit() {}
+
+  onSubmit() {
+    if (this.productoForm.valid) {
+      // lógica para enviar el producto cuando el formulario es válido
+      console.log('Formulario válido', this.productoForm.value);
+    } else {
+      // muestra un mensaje de error si el formulario es inválido
+      console.log('Formulario inválido');
+    }
+  }
+
+  // método para acceder fácilmente a los controles del formulario desde el html
+  get formControls() {
+    return this.productoForm.controls;
+  }
 }
